3a3f859fa3b87c040e74e184f89c7b42368d7d0b
taiselopes <taiselopes21@gmail.com>
2020-04-18 21:13:39 +0000

    Created function to get episodes by ID

--- mob-dev-CA2/src/app/pages/episodes/episodes.page.ts
+++ mob-dev-CA2/src/app/pages/episodes/episodes.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-episodes',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodesPage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController, private router: Router) { }
 
   ngOnInit() {
   }
