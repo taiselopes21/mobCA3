a08fc6facd0a288345df122f5d1373539df1d338
taiselopes <taiselopes21@gmail.com>
2020-04-19 15:07:23 +0000

    improved death count

--- mob-dev-CA2/src/app/pages/deaths/deaths.page.ts
+++ mob-dev-CA2/src/app/pages/deaths/deaths.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-deaths',
-  templateUrl: './deaths.page.html',
-  styleUrls: ['./deaths.page.scss'],
+    selector: 'app-deaths',
+    templateUrl: './deaths.page.html',
+    styleUrls: ['./deaths.page.scss'],
 })
 export class DeathsPage implements OnInit {
+    
+    deaths: Observable<any>;
 
-  constructor() { }
+    constructor(private router: Router, private api: ApiService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.deaths = this.api.getDeaths();
+        this.deaths.subscribe(data => {
+        console.log('my data: ', data);
+        });
+    }
 
+    
 }
+
+    
+
+
