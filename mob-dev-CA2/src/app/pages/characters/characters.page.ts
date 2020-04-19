19d55a7443e739096a73d33e08aaedc8bd680106
taiselopes <taiselopes21@gmail.com>
2020-04-19 14:50:48 +0000

    API service characters list working

--- mob-dev-CA2/src/app/pages/characters/characters.page.ts
+++ mob-dev-CA2/src/app/pages/characters/characters.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
+ 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
   styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
-
-  constructor() { }
-
+ 
+  characters: Observable<any>;
+ 
+  constructor(private router: Router, private api: ApiService) { }
+ 
   ngOnInit() {
+    this.characters = this.api.getCharacters();
+    this.characters.subscribe(data => {
+    console.log('my data: ', data);
+    });
   }
-
+ 
+  openDetails(character) {
+    let split = character.url.split('/');
+    let characterId = character.char_id;
+    this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+  }
 }
