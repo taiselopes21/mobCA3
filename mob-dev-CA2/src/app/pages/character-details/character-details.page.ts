e1b403e004602b4c39ae193fabf597fd1c8fc4ff
taiselopes <taiselopes21@gmail.com>
2020-04-25 12:37:57 +0000

    improved characters and episodes details

--- mob-dev-CA2/src/app/pages/character-details/character-details.page.ts
+++ mob-dev-CA2/src/app/pages/character-details/character-details.page.ts
@@ -1,19 +1,32 @@
-import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.page.html',
   styleUrls: ['./character-details.page.scss'],
 })
 export class CharacterDetailsPage implements OnInit {
-  character: any;
-  characterId = null;
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
-  ngOnInit() {
-    this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getCharacter(this.characterId).subscribe(res => {
-      this.character = res[0];
-    })
-    }
+
+  character: Observable<any>;
+    characterId = null;
+    icon:any = false;
+    constructor(private activatedRoute: ActivatedRoute,
+        private api: ApiService) { }
+
+    ngOnInit() {
+        this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getCharacter(this.characterId).subscribe(res => {
+            this.character = res[0];
+            console.log(JSON.stringify(this.characterId));
+            console.log(this.character);
+        });
+      
+      }
+
+    //   ionViewWillEnter() {
+       
+
+
 }
--- mob-dev-CA2/src/app/pages/episode-details/episode-details.page.ts
+++ mob-dev-CA2/src/app/pages/episode-details/episode-details.page.ts
@@ -1,19 +1,31 @@
-import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+
 @Component({
-  selector: 'app-episode-details',
-  templateUrl: './episode-details.page.html',
-  styleUrls: ['./episode-details.page.scss'],
+    selector: 'app-episode-details',
+    templateUrl: './episode-details.page.html',
+    styleUrls: ['./episode-details.page.scss'],
 })
 export class EpisodeDetailsPage implements OnInit {
-  episode: any;
-  episodeId = null;
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
-  ngOnInit() {
-    this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getEpisode(this.episodeId).subscribe(res => {
-      this.episode = res[0];
-    })
+
+    episode: Observable<any>;
+    episodeId = null;
+    icon: any = false;
+    constructor(private activatedRoute: ActivatedRoute,
+        private api: ApiService) { }
+
+    ngOnInit() {
+
+    }
+    ionViewWillEnter() {
+        this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getEpisode(this.episodeId).subscribe(res => {
+            this.episode = res[0];
+            console.log(JSON.stringify(this.episodeId.id));
+        });
+
+
     }
 }
