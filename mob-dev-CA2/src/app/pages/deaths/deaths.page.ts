60a8cf400d4807cc05dc8ea42d4e5e67e2b89063
taiselopes <taiselopes21@gmail.com>
2020-04-25 18:41:43 +0000

    deaths search implemented

--- mob-dev-CA2/src/app/pages/deaths/deaths.page.ts
+++ mob-dev-CA2/src/app/pages/deaths/deaths.page.ts
@@ -12,11 +12,20 @@ import { ApiService } from '../../services/api.service';
 export class DeathsPage implements OnInit {
  
   deaths: Observable<any>;
- 
+  search:any = "";
   constructor(private router: Router, private api: ApiService) { }
  
   ngOnInit() {
     this.deaths = this.api.getDeaths();
+    this.deaths.subscribe(data => {
+      console.log('my data: ', data);
+      });
+  }
+
+  filter(death){
+    // console.log(death.author);
+    return death.responsible.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
   }
 }
+
 
